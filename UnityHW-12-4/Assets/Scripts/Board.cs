21887933292b00ff339aca28b534247aa3e1d4c0
CubeDr <spaceship00@naver.com>
2017-12-07 15:58:18 +0900

    Board holds dragged monster

---
+++ UnityHW-12-4/Assets/Scripts/Board.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour {
+    public DragAndDropSystem system;
+
+    public Color defaultColor = Color.white;
+    public Color highlightedColor = Color.green;
+    private new SpriteRenderer renderer;
+
+    private GameObject _monster;
+    public GameObject monster {
+        get {
+            return _monster;
+        }
+
+        set {
+            if (_monster) Destroy(_monster);
+            _monster = value;
+            _monster.transform.position = transform.position + Vector3.back;
+        }
+    }
+
+    // Use this for initialization
+    void Start() {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Highlight(bool highlight) {
+        renderer.color = highlight ? highlightedColor : defaultColor;
+    }
+}
--- UnityHW-12-4/Assets/Scripts/DragAndDropSystem.cs
+++ UnityHW-12-4/Assets/Scripts/DragAndDropSystem.cs
@@ -2,31 +2,59 @@
 
 public class DragAndDropSystem : MonoBehaviour {
 
-    private GameObject prefabUI;
+    private GameObject ui;
     private GameObject prefab;
     public Canvas canvas;
 
-    private bool isDragging = false;
+    private Board prev;
+    private Board now;
+
+    private bool _isDragging = false;
+    public bool isDragging {
+        get {
+            return _isDragging;
+        }
+    }
 
 	// Update is called once per frame
 	void Update () {
-        if (!isDragging) return;
+        if (!_isDragging) return;
 
-        prefabUI.transform.position = Input.mousePosition;
-        if (Input.GetMouseButtonUp(0)) EndDrag();
-	}
+        ui.transform.position = Input.mousePosition;
 
-    private void EndDrag() {
-        isDragging = false;
-        Destroy(prefabUI);
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        pos.z = 0;
-        Instantiate(prefab, pos, Quaternion.identity);
+        Vector3 origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(origin.x, origin.y), Vector2.zero, 100f, 1 << 8);
+        if (hit) {
+            now = hit.transform.GetComponent<Board>();
+            if (prev) {
+                if (now != prev) {
+                    prev.Highlight(false);
+                    now.Highlight(true);
+                }
+            } else now.Highlight(true);
+        } else if (prev) {
+            prev.Highlight(false);
+            now = null;
+        } else now = null;
+        prev = now;
+
+        if(Input.GetMouseButtonUp(0)) EndDrag();
+    }
+
+    void EndDrag() {
+        _isDragging = false;
+        Destroy(ui);
+        if(now) {
+            now.monster = Instantiate(prefab) as GameObject;
+            now.Highlight(false);
+            now = null;
+        }
     }
 
     public void StartDrag(GameObject prefabUI, GameObject prefab) {
-        this.prefabUI = Instantiate(prefabUI, Input.mousePosition, Quaternion.identity, canvas.transform) as GameObject;
+        this.ui = Instantiate(prefabUI, Input.mousePosition, Quaternion.identity, canvas.transform) as GameObject;
         this.prefab = prefab;
-        isDragging = true;
+        _isDragging = true;
     }
+
 }
