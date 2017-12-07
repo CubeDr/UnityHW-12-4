de0fc0c6077b1495b4590e3de6a1c2e801a82b9e
CubeDr <spaceship00@naver.com>
2017-12-07 12:08:00 +0900

    ui drags, add background camera

---
+++ UnityHW-12-4/Assets/Scripts/DragAndDropButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropButton : MonoBehaviour, IPointerDownHandler {
+    public DragAndDropSystem system;
+
+    public GameObject spawningMonsterUI;
+    public GameObject spawningMonster;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerDown(PointerEventData data) {
+        system.StartDrag(spawningMonsterUI, spawningMonster);
+    }
+}
---
+++ UnityHW-12-4/Assets/Scripts/DragAndDropSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DragAndDropSystem : MonoBehaviour {
+
+    private GameObject prefabUI;
+    private GameObject prefab;
+    public Canvas canvas;
+
+    private bool isDragging = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isDragging) return;
+
+        prefabUI.transform.position = Input.mousePosition;
+        if (Input.GetMouseButtonUp(0)) EndDrag();
+	}
+
+    private void EndDrag() {
+        isDragging = false;
+        Destroy(prefabUI);
+    }
+
+    public void StartDrag(GameObject prefabUI, GameObject prefab) {
+        this.prefabUI = Instantiate(prefabUI, Input.mousePosition, Quaternion.identity, canvas.transform) as GameObject;
+        this.prefab = prefab;
+        isDragging = true;
+    }
+}
